b9a5a3ee26c252748bbe8fbc6bf794c1a125fdcd
ladypinetree <maureenpine@gmail.com>
2017-03-21 12:13:54 -0400

    Hover and text decoration for list and list items

--- app/components/listr-list.js
+++ app/components/listr-list.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  //put in camelcase
+  classNameBindings: ['listDetailHidden'],
+  listDetailHidden: true,
+  actions: {
+    toggleListDetail () {
+      return this.toggleProperty('listDetailHidden');
+    },
+  },
 });
--- app/components/listr-list/item.js
+++ app/components/listr-list/item.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  //put in camelcase
+  classNameBindings: ['strikethrough'],
+  actions: {
+    markComplete: function () {
+      this.toggleProperty('strikethrough');
+    },
+  },
 });
